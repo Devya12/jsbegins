d0365a8dcf066eebf67613897244651ee9b63fa9
Divya <ashiyadivya1208@gmail.com>
2025-06-21 12:12:02 +0530

    dynamic listing

--- jsbegin.js
+++ jsbegin.js
@@ -1,1 +1,22 @@
-console.log("This is divya and my age is", 21 + 5);
+let arrlist =["item1","item2","item3","item4"];
+currindex=0;
+function buttonpress(){
+  let name= document.getElementById("newlist");   
+  
+ if(currindex<arrlist.length){
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(arrlist[currindex]));
+    name.appendChild(li)
+currindex++
+
+}
+
+}
+function deleteonpress()
+{
+    let deletel = document.getElementById("newlist");
+    if(deletel.hasChildNodes)
+{
+    deletel.removeChild(deletel.lastChild)
+    currindex--;
+}}
